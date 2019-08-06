6555335d7d466fc82df3bea395205a8a0351b961
Luke <luke@Kellys-MacBook-Pro.local>
2019-08-06 20:44:42 +0100

    allow cors

--- server.js
+++ server.js
@@ -15,8 +15,13 @@ const s3 = new AWS.S3();
 
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.static('public'));
 
-app.use(express.static('public'))
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
